a91fabd463cfefd001d986af9af5f071de5c5a14
Stefan Stoyanovich <stefanstoya@gmail.com>
2022-06-25 21:35:06 -0400

    WIP

--- pages/problems.vue
+++ pages/problems.vue
@@ -1,7 +1,22 @@
 <template>
-    <div class="mx-auto container mt-16">
-        <p>
-            Due to budgetary reasons, Quality Assurance is often neglected until the company starts seeing either critical quality issues such as customer complaints and/or large number of bugs being filed. A lot of this could be prevented with a well defined QA Process/Procedure and well written Automation Tests. Even companies who allocate sufficient or large budgets for QA could run into challenges when they don't have and/or follow good QA Strategy. This includes not adopting automation and overworking Manual QA, poorly written automation tests, testing with the incorrect type of tests (see Testing Pyramid), lack of documentation, and using strategy not suited for the company.
-        </p>
+    <div class=" text-gray-700">
+        <div class="flex">
+            <div class="w-1/2 bg-cover" style="background-image: url('/assets/images/laptop.jpg');">
+            </div>
+            <div class="w-1/2 bg-black mx-auto px-16 py-16">
+            <h1 class="text-3xl text-white">The Problem</h1>
+                <p class="text-white subText">
+                    Due to budgetary reasons, Quality Assurance is often neglected until the company starts
+                    seeing either critical quality issues such as customer complaints and/or large number of 
+                    bugs being filed.
+                </p>
+            </div>
+        </div>
+        <p>A lot of this could be prevented with a well defined QA Process/Procedure 
+                    and well written Automation Tests. Even companies who allocate sufficient or large budgets 
+                    for QA could run into challenges when they don't have and/or follow good QA Strategy. 
+                    This includes not adopting automation and overworking Manual QA, poorly written automation 
+                    tests, testing with the incorrect type of tests (see Testing Pyramid), lack of documentation, 
+                    and using strategy not suited for the company.</p>
     </div>
 </template>
