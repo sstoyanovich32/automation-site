7b6bf5d5737dabb3fc7f7054bfd7489bf3fa51d2
sstoyanovich32 <stefanstoya@gmail.com>
2022-08-31 19:17:52 -0400

    will this work

--- components/home/technology.vue
+++ components/home/technology.vue
@@ -6,83 +6,83 @@
       >
       <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/selenium.png"
+          src="../../public/images/selenium.png"
           alt="Selenium"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/python.png"
+          src="../../public/images/python.png"
           alt="Python"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/github.png"
+          src="../../public/images/github.png"
           alt="Github"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/javascript.png"
+          src="../../public/images/javascript.png"
           alt="Javascript"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/puppeteer.png"
+          src="../../public/images/puppeteer.png"
           alt="Puppeteer"
         />
         
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/html5.png"
+          src="../../public/images/html5.png"
           alt="HTML5"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/css.png"
+          src="../../public/images/css.png"
           alt="CSS"
         />
         <img
           class="h-16 reveal hover:scale-125"
-          src="~/public/images/cypress.png"
+          src="../../public/images/cypress.png"
           alt="Cypress.io"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/nuxt.png"
+          src="../../public/images/nuxt.png"
           alt="Nuxt"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/react.png"
+          src="../../public/images/react.png"
           alt="React"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/node.png"
+          src="../../public/images/node.png"
           alt="Node"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/php.png"
+          src="../../public/images/php.png"
           alt="PHP"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/laravel.png"
+          src="../../public/images/laravel.png"
           alt="Laravel"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/vue.png"
+          src="../../public/images/vue.png"
           alt="Vue"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/wordpress.png"
+          src="../../public/images/wordpress.png"
           alt="Wordpress"
         />
         <img
           class="w-16 reveal hover:scale-125"
-          src="~/public/images/cucumber.svg"
+          src="../../public/images/cucumber.svg"
           alt="Cucumber"
         />
       </div>
