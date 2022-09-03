b8763e9c6bcad85cf08fddf3dbdbae878d8d06f7
sstoyanovich32 <stefanstoya@gmail.com>
2022-09-02 22:52:28 -0400

    updating config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
-import { defineNuxtConfig } from 'nuxt'
-import { resolve } from 'path'
+import { defineNuxtConfig } from "nuxt";
+import { resolve } from "path";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
-    publicRuntimeConfig: {
-        BASE_URL: process.env.BASE_URL
-    },
-    alias: {
-        'images': resolve(__dirname, '/assets/images'),
-      }
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  publicRuntimeConfig: {
+    BASE_URL: process.env.BASE_URL,
+  },
+  alias: {
+    images: resolve(__dirname, "/assets/images"),
+  },
+  nitro: {
+    preset: "node-server",
+  },
+});
